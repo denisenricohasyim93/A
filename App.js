20ec79fe9cc79e60f26e4482cddc071d2cc3f0e6
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2017-08-18 15:49:53 +0700

    D
    D

--- App.js
+++ App.js
@@ -1,10 +1,97 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { AppRegistry, View, Image } from 'react-native';
+import $ from 'jquery';
 
 export default class HelloWorldApp extends Component {
+  $.ajax('http://jsonplaceholder.typicode.com/posts/1', {
+    method: 'GET'
+  }).then(function(data) {
+    console.log(data);
+  });
+  
+  $.ajax('http://jsonplaceholder.typicode.com/posts', {
+  method: 'GET'
+  }).then(function(data) {
+  console.log(data);  
+  });
+  
+  // POST adds a random id to the object sent
+  $.ajax('http://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    data: {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }
+  }).then(function(data) {
+    console.log(data);
+  });
+
+  /* will return
+  {
+    id: 101,
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+  }
+  */
+
+  $.ajax('http://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PUT',
+    data: {
+      id: 1,
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }
+  }).then(function(data) {
+    console.log(data);
+  });
+
+  /* will return
+  {
+    id: 1
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+  }
+  */
+
+  $.ajax('http://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PATCH',
+    data: {
+      title: 'foo'
+    }
+  }).then(function(data) {
+    console.log(data);
+  });
+
+  /* will return
+  {
+    id: 1
+    title: 'foo',
+    body: 'quia et suscipit [...]',
+    userId: 1
+  }
+  */
+
+  $.ajax('http://jsonplaceholder.typicode.com/posts/1', {
+  method: 'DELETE'
+  });
+
+  // Will return all the posts that belong to the first user
+  $.ajax('http://jsonplaceholder.typicode.com/posts?userId=1').then(function(data) {
+    console.log(data);
+  });
+
+  $.ajax('http://jsonplaceholder.typicode.com/posts/1/comments').then(function(data) {
+    console.log(data);
+  });
+  // Which is equivalent to /comments?postId=1
+
   render() {
-    return (
+      return (
       <View>
         <Text style={{fontWeight: 'bold'}}>Denis Enrico Hasyim</Text>
         <Image
@@ -16,6 +103,8 @@ export default class HelloWorldApp extends Component {
     );
   }
 }
+
+
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
